92e05d504bea3c95264bf93914f43854673a2012
jduggan1 <jduggan1@github.com>
2014-11-19 01:50:38 -0500

    finished nFollowers script

---
+++ nFollowers.py
@@ -0,0 +1,16 @@
+import pymongo, json
+
+users = {}
+client = pymongo.MongoClient(host='da0.eecs.utk.edu')
+db = client['bitbucket']
+followers = db['followers']
+
+iterator = followers.find({})
+for result in iterator:
+	try:
+		username = result['url'].lstrip('https://bitbucket.org/api/2.0/users/').rstrip('/followers')
+		users[username] = len(result['values'])
+	except KeyError:
+		pass
+
+print json.dumps(users)

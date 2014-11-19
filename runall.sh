5e9cdd16afa2a2073385c50288dd608dc450578d
jduggan1 <jduggan1@github.com>
2014-11-19 01:39:20 -0500

    updated runall script

--- runall.sh
+++ runall.sh
@@ -2,3 +2,5 @@
 nohup python nCommits.py > data/nCommits.data 2> data/nCommits.err &
 nohup python fqCommits.py > data/fqCommits.data 2> data/fqCommits.err &
 nohup python nForks.py > data/nForks.data 2> data/nForks.err &
+nohup python timeContrb.py > data/timeContrb.data 2> data/timeContrb.err &
+nohup python totalRepos.py > data/totalRepos.data 2> data/totalRepos.err &
